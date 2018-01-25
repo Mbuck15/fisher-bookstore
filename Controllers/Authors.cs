bbcdb163e59177133aee0fbe148de3356cc4ffb8
Mason Buck <buck.217@osu.edu>
2018-01-24 22:32:22 -0500

    Added routing and controllers

---
+++ Controllers/Authors.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        public IActionResult Featured()
+        {
+            return Content ("this is the authors featured controller");
+        }
+    }
+}
